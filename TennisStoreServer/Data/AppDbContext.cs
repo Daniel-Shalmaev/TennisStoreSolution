0753565594e9e108e2015888cf00c2745eba2ed8
Daniel-Shalmaev <Danielshalmaev2015@gmail.com>
2024-10-20 16:57:53 +0300

    First Migration

---
+++ TennisStoreServer/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TennisStoreSharedLibrary.Models;
+
+namespace TennisStoreServer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)"); // Specify precision and scale
+        }
+
+
+    }
+}
--- TennisStoreServer/Program.cs
+++ TennisStoreServer/Program.cs
@@ -1,25 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using TennisStoreServer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure the database context with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")
+        ?? throw new InvalidOperationException("Connection string not found"));
+});
+
 var app = builder.Build();
+
+//// Apply pending migrations and create the database if it does not exist
+//using (var scope = app.Services.CreateScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    dbContext.Database.EnsureCreated(); // Create the database if it doesn't exist
+//    dbContext.Database.Migrate();       // Apply pending migrations
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
